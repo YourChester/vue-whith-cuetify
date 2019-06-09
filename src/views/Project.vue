5fbd8425cfb8150dc0595154702aa1cb655aaeda
Каменев Артур Романович <artyrkamenev@gmail.com>
2019-06-09 22:01:09 +0300

    Second commit

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="project">
-    <h1>Project</h1>
+    <h1 class="subheading grey--text">Project</h1>
+
+    <v-container class="my-5">
+      
+    </v-container>
+    
   </div>
 </template>
 
